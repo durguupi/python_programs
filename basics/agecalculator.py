9080421d22ff26563674e487296485f2558fdfa3
durguupi <durguupi@gmail.com>
2021-01-01 19:35:14 +0530

    first commit

---
+++ basics/agecalculator.py
@@ -0,0 +1,10 @@
+# 🚨 Don't change the code below 👇
+age = input("What is your current age? ")
+# 🚨 Don't change the code above 👆
+
+# Write your code below this line 👇
+remage = 90 - int(age)
+days = remage * 365
+weeks = remage * 52
+years = remage * 12
+print(f"You have {days} days, {weeks} weeks and {years} months left")
---
+++ basics/twodigitadd.py
@@ -0,0 +1,11 @@
+# 🚨 Don't change the code below 👇
+two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+# Write your code below this line 👇
+# Get the subscritping and then add the output
+num1 = two_digit_number[0]
+num2 = two_digit_number[1]
+# Since the type is string we are converting to integer
+print(int(num1) + int(num2))
